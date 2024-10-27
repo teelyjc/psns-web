d96701d78fb290932fa3defdbf32b1c9cdb03e19
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-10-27 22:20:28 +0700

    feat: change file name to url path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,16 @@
-import type { FunctionComponent } from "react";
 import { Fragment } from "react";
 import { Container } from "react-bootstrap";
 
-import Navbar from "@/components/common/Navbar";
+import Navbar from "@/components/common/navbar-nav";
 
-const HomePage: FunctionComponent = () => {
+export default function Index() {
   return (
     <Fragment>
       <Navbar />
+
       <Container className="mt-5">
         <h1>ยินดีต้อนรับสู่ Pet-System</h1>
       </Container>
     </Fragment>
   );
-};
-
-export default HomePage;
+}
